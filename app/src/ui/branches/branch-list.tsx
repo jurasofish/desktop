b8ee4ba6d7dca8466e78358fe86c31e314ad172a
Michael Jurasovic <michael.jurasovic@orkestra.energy>
2026-07-10 07:18:25 +1000

    Create branches from selected branches

--- app/src/ui/branches/branch-list.tsx
+++ app/src/ui/branches/branch-list.tsx
@@ -134,6 +134,8 @@ interface IBranchListProps {
 
   /** Optional: Callback to checkout a branch in a new worktree */
   readonly onCheckoutInNewWorktree?: (branch: Branch) => void
+
+  readonly onCreateBranchFromBranch?: (branchName: string) => void
 }
 
 interface IBranchListState {
@@ -286,13 +288,18 @@ export class BranchList extends React.Component<
   ) => {
     event.preventDefault()
 
-    const { onRenameBranch, onDeleteBranch, onCheckoutInNewWorktree } =
-      this.props
+    const {
+      onRenameBranch,
+      onDeleteBranch,
+      onCheckoutInNewWorktree,
+      onCreateBranchFromBranch,
+    } = this.props
 
     if (
       onRenameBranch === undefined &&
       onDeleteBranch === undefined &&
-      onCheckoutInNewWorktree === undefined
+      onCheckoutInNewWorktree === undefined &&
+      onCreateBranchFromBranch === undefined
     ) {
       return
     }
@@ -302,6 +309,7 @@ export class BranchList extends React.Component<
     const items = generateBranchContextMenuItems({
       branch,
       onRenameBranch,
+      onCreateBranchFromBranch,
       onDeleteBranch,
       onCheckoutInNewWorktree,
     })
